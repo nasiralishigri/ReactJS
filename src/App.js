bc69787a36986e38eb2a2c91fb8d922a71486e9d
nasiralishigri <nasiralishigri786@gmail.com>
2019-09-17 13:11:02 +0500

    react bootstrap reactStrap configure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Navbar , NavbarBrand} from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <Navbar dark color="primary">
+
+<div className="container">
+<NavbarBrand href="/">Still Confusion in ReactJS Dev</NavbarBrand>
+
+</div>
+
+      </Navbar>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
