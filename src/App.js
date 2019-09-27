acce96453dca09d47f7483259787e59e0337d524
nasiralishigri <nasiralishigri786@gmail.com>
2019-09-27 18:12:49 +0500

    Component Part 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
 import {Navbar , NavbarBrand} from 'reactstrap';
-import './menuComponent.js';
-import Menu from './menuComponent.js';
-function App() {
+import Menu from './components/menuComponent.js';
+import './App.css';
+import { DISHES } from './shared/dishes';
+class App extends Component {
+ 
+    constructor(props) {
+      super(props);
+      this.state = {
+        dishes: DISHES
+      };
+    }
+ render(){
   return (
-    <div className="App">
-
+    <div>
       <Navbar dark color="primary">
-
-<div className="container">
-<NavbarBrand href="/">Still Confusion in ReactJS Dev</NavbarBrand>
-
-</div>
-
+            <div className="container">
+            <NavbarBrand href="/">Still Confusion in ReactJS Dev</NavbarBrand>
+            </div>
       </Navbar>
+      <Menu dishes={this.state.dishes} />
 
-
-     <Menu/>>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
+    }
 }
 
 export default App;
